566d6c07b8a45f36dffd8488ec235721ecb3f316
Ritambrata Karak <47169972+ritambratakarak@users.noreply.github.com>
2024-09-10 13:03:33 +0530

    Update API.ts

--- API.ts
+++ API.ts
@@ -1,8 +1,6 @@
 export const Google_API_KEY1 = 'AIzaSyAiRo8qHObkdHDyndeOJU9PMwLfTFHEkLY'
-export const Google_API_KEY2 = 'AIzaSyAiRo8qHObkdHDyndeOJU9PMwLfTFHEkLY'
+export const Google_API_KEY2 = 'AIzaSyCFoG2Z5L8b3IA5I5JKkCu_zgIwhky4Ang'
 export const APPWRITE = {
     ENDPOINT: 'https://cloud.appwrite.io/v1',
     PROJECT_ID: '66cffdf3003a4d290e06'
 };
-
- 
